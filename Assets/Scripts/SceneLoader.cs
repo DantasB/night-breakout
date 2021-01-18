d89acf35528da461e4b8f8c32a6804879f943972
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-01-18 11:44:19 -0300

    [Scripts] Adding Scripts
    I've learned how to import scripts from another project, so i've imported some old scripts.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(GetCurrentSceneIndex() + 1);
+    }
+
+    private int GetCurrentSceneIndex()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        TreatIndex(ref currentSceneIndex);
+
+        return currentSceneIndex;
+    }
+
+    private void TreatIndex(ref int currentSceneIndex)
+    {
+        if (currentSceneIndex == 2)
+        {
+            currentSceneIndex = -1;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
